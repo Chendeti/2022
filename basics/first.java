d20ed7549252efd6d059f7db80b0d5972cbc8e7b
Vamsi Chendeti <v.chendeti@zensar.com>
2022-04-23 10:41:39 +0530

    first

---
+++ basics/first.java
@@ -0,0 +1,12 @@
+package basics;
+public class first {
+    public static void main(String[] args) {
+         double a = 50.9;
+         int b = -6;
+
+         System.out.println("Sum =" + (a+b));
+         System.out.println("Subtraction =" + (a-b));
+         System.out.println("Divison =" + (a/b));
+         System.out.println("Product =" + (a*b));
+    }     
+}
