3cf69ddfeb3bd9be4b5bed1342a9139ce9c5c20e
Vamsi Chendeti <v.chendeti@zensar.com>
2022-04-23 13:13:30 +0530

    first

---
+++ basics/datatypes.java
@@ -0,0 +1,16 @@
+package basics;
+
+public class datatypes {
+    public static void main(String[] args) {
+        
+        int a = -12;
+
+        double d = 45.6;
+
+        String s = "Aaryan is the Trainer";g
+
+        char ch = 'a';
+
+        boolean bool = true;
+    }
+}
