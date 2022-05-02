09c416c1fe6a204f8bd33301dfacfb0e5848e527
Vamsi Chendeti <v.chendeti@zensar.com>
2022-05-02 11:24:49 +0530

    first

---
+++ basics/core_java/inex.java
@@ -0,0 +1,17 @@
+package basics.core_java;
+
+@FunctionalInterface
+public interface inex {
+    public void start();
+}
+
+class impl implements inex {
+     
+    @Override
+    public void start() {
+        System.out.println("Hello");
+
+    }
+
+}
+    
